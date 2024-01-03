4fcc0786798c524c1724e80ee5ad4061d86862f9
Jorge Eduardo Tamayo Ramos <JorgeT@digitalware.com.co>
2024-01-03 16:53:48 -0500

    Add project files.

---
+++ Envelope.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlReaderTest
+{
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.xmlsoap.org/soap/envelope/", IsNullable = false)]
+    public partial class Envelope
+    {
+
+        private EnvelopeHeader headerField;
+
+        private EnvelopeBody bodyField;
+
+        private string encodingStyleField;
+
+        /// <remarks/>
+        public EnvelopeHeader Header
+        {
+            get
+            {
+                return this.headerField;
+            }
+            set
+            {
+                this.headerField = value;
+            }
+        }
+
+        /// <remarks/>
+        public EnvelopeBody Body
+        {
+            get
+            {
+                return this.bodyField;
+            }
+            set
+            {
+                this.bodyField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute(Form = System.Xml.Schema.XmlSchemaForm.Qualified)]
+        public string encodingStyle
+        {
+            get
+            {
+                return this.encodingStyleField;
+            }
+            set
+            {
+                this.encodingStyleField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public partial class EnvelopeHeader
+    {
+
+        private Transaction transactionField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "xxs")]
+        public Transaction Transaction
+        {
+            get
+            {
+                return this.transactionField;
+            }
+            set
+            {
+                this.transactionField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "xxs")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "xxs", IsNullable = false)]
+    public partial class Transaction
+    {
+
+        private tc tcField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "")]
+        public tc tc
+        {
+            get
+            {
+                return this.tcField;
+            }
+            set
+            {
+                this.tcField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class tc
+    {
+
+        private string pidField;
+
+        private string tidField;
+
+        private System.DateTime dtField;
+
+        /// <remarks/>
+        public string pid
+        {
+            get
+            {
+                return this.pidField;
+            }
+            set
+            {
+                this.pidField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string tid
+        {
+            get
+            {
+                return this.tidField;
+            }
+            set
+            {
+                this.tidField = value;
+            }
+        }
+
+        /// <remarks/>
+        public System.DateTime dt
+        {
+            get
+            {
+                return this.dtField;
+            }
+            set
+            {
+                this.dtField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public partial class EnvelopeBody
+    {
+
+        private XXTransactionResponse xXTransactionResponseField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "xxs")]
+        public XXTransactionResponse XXTransactionResponse
+        {
+            get
+            {
+                return this.xXTransactionResponseField;
+            }
+            set
+            {
+                this.xXTransactionResponseField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "xxs")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "xxs", IsNullable = false)]
+    public partial class XXTransactionResponse
+    {
+
+        private RSP rSPField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "")]
+        public RSP RSP
+        {
+            get
+            {
+                return this.rSPField;
+            }
+            set
+            {
+                this.rSPField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class RSP
+    {
+
+        private RSPAirShoppingRS airShoppingRSField;
+
+        /// <remarks/>
+        public RSPAirShoppingRS AirShoppingRS
+        {
+            get
+            {
+                return this.airShoppingRSField;
+            }
+            set
+            {
+                this.airShoppingRSField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class RSPAirShoppingRS
+    {
+
+        private RSPAirShoppingRSShoppingResponseID shoppingResponseIDField;
+
+        private decimal versionField;
+
+        private string transactionIdentifierField;
+
+        /// <remarks/>
+        public RSPAirShoppingRSShoppingResponseID ShoppingResponseID
+        {
+            get
+            {
+                return this.shoppingResponseIDField;
+            }
+            set
+            {
+                this.shoppingResponseIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal Version
+        {
+            get
+            {
+                return this.versionField;
+            }
+            set
+            {
+                this.versionField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string TransactionIdentifier
+        {
+            get
+            {
+                return this.transactionIdentifierField;
+            }
+            set
+            {
+                this.transactionIdentifierField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    public partial class RSPAirShoppingRSShoppingResponseID
+    {
+        /// <remarks/>
+        public string Owner { get; set; }
+
+        /// <remarks/>
+        public string ResponseID { get; set; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string Source { get; set; }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Xml;
+using System.Xml.Serialization;
+using XmlReaderTest;
+
+
+var xmlContent = File.ReadAllText("EresElMejor.xml");
+
+// XML content (replace with your XML content or file path)
+//string xmlContent = "<Person><FirstName>John</FirstName><LastName>Doe</LastName><Age>30</Age></Person>";
+
+// Deserialize XML into object
+Envelope xml;
+using (StringReader stringReader = new StringReader(xmlContent))
+{
+    XmlSerializer serializer = new XmlSerializer(typeof(Envelope));
+    xml = (Envelope)serializer.Deserialize(stringReader);
+}
+
+// Access the deserialized object
+Console.WriteLine($"Owner: {xml.Body.XXTransactionResponse.RSP.AirShoppingRS.ShoppingResponseID.Owner}");
+Console.WriteLine($"ResponseID: {xml.Body.XXTransactionResponse.RSP.AirShoppingRS.ShoppingResponseID.ResponseID}");
+Console.WriteLine($"Source: {xml.Body.XXTransactionResponse.RSP.AirShoppingRS.ShoppingResponseID.Source}");
+
+Console.ReadLine();
